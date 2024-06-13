1faa30603ebe478e17bcda072d651dbfe72b39bf
Iago Real <iagovreal@gmail.com>
2024-06-13 13:28:52 -0300

    first commit

---
+++ src/model/agenda.model.ts
@@ -0,0 +1,8 @@
+export class schedule{
+    constructor(
+        public name: string,
+        public email: string,
+        public phone: string,
+        public adress: string
+    ) {}
+}
---
+++ src/service/rest/agenda.rest.ts
@@ -0,0 +1,5 @@
+import api from "../config"
+
+export class agendaRest{
+    
+}
